ae98905d1952099e086d7a6378cdb6f59f47fd82
Pawel Dziagwa <pawe0918@edu.zealand.dk>
2022-12-13 18:28:47 +0100

    v1

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
+const baseurl = "localhost/BarcodeBackend"
 
 Vue.createApp({
     data() {
       return {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        shoppingCarts: []
+      }
+    },
+    methods:{
+      async getShoppingCarts(){
+        url = baseurl + "/ShoppingCarts"
+        try{
+          const response = await axios.get(url)
+          this.shoppingCarts = await response.data
+        }
+        catch(ex){
+          console.log(ex.message)
+        }
       }
     }
 }).mount('#app')
